96ab21eb6ebeb2608d54725ece95612932fcca70
alisuehobbs <alisuehobbs@gmail.com>
2016-11-13 13:05:43 -0700

    set up map in main

--- client/javascripts/controllers/main_controller.js
+++ client/javascripts/controllers/main_controller.js
@@ -1,3 +1,23 @@
-app.controller('MainController', function ($scope, $routeParams, $location, $cookies) {
-  
+app.controller('MainController', function(esriLoader, $scope, $routeParams, $location, $cookies) {
+    var view;
+    require([
+        "esri/Map",
+        "esri/views/SceneView",
+        "dojo/domReady!"
+    ], function(Map, SceneView) {
+
+        var map = new Map({
+            basemap: "streets"
+        })
+
+        view = new SceneView({
+            container: "viewDiv",
+            map: map,
+            center: [-101.17, 21.78],
+            scale: 50000000
+        })
+
+    })
+
+
 })
