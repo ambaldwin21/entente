ca913a42238d82f810aa39a115a3592b05949b19
alisuehobbs <alisuehobbs@gmail.com>
2016-11-14 15:20:40 -0700

    added logout functionality on profile. hid forms on questions

--- client/javascripts/controllers/profile_controller.js
+++ client/javascripts/controllers/profile_controller.js
@@ -1,9 +1,17 @@
 app.controller('ProfileController', function($scope, $routeParams, $location, $cookies) {
 
-    $scope.user = $cookies.getObject('loggedin')
+    $scope.errorMessage = false
 
-    var view;
-    require([
+    var cookie = $cookies.getObject('loggedin')
+
+    if(!cookie) {
+      $scope.errorMessage = true
+      $scope.error = "You must be logged in to view this page"
+    } else {
+      $scope.user = cookie
+
+      var view;
+      require([
         // ArcGIS JS
         "esri/views/MapView",
         "esri/Map",
@@ -11,47 +19,55 @@ app.controller('ProfileController', function($scope, $routeParams, $location, $c
         "dojo/query",
         "esri/widgets/Search",
         "dojo/domReady!"
-    ], function(MapView, Map, Legend, query, Search) {
+      ], function(MapView, Map, Legend, query, Search) {
 
         var map = new Map({
-            basemap: "streets",
-            ground: "world-elevation"
+          basemap: "streets",
+          ground: "world-elevation"
         })
 
         // View
         var view = new MapView({
-            map: map,
-            container: "profileMap",
-            padding: {
-                top: 50
-            },
-            scale: 50000000,
-            center: [-101.17, 21.78]
+          map: map,
+          container: "profileMap",
+          padding: {
+            top: 50
+          },
+          scale: 50000000,
+          center: [-101.17, 21.78]
         });
 
         // Legend
         view.then(function(result) {
-            var legend = new Legend({
-                view: view,
-                layerInfos: [{
-                    layer: view.map.layers.items[0],
-                    title: ""
-                }]
-            });
-            view.ui.add(legend, "top-right");
-            query("#" + legend.id).addClass("collapse in");
+          var legend = new Legend({
+            view: view,
+            layerInfos: [{
+              layer: view.map.layers.items[0],
+              title: ""
+            }]
+          });
+          view.ui.add(legend, "top-right");
+          query("#" + legend.id).addClass("collapse in");
         });
 
         var searchWidget = new Search({
-            view: view
+          view: view
         })
 
         searchWidget.startup()
 
         view.ui.add(searchWidget, {
-            position: "top-left",
-            index: 0
+          position: "top-left",
+          index: 0
         })
 
-    })
+      })
+
+      $scope.logout = function () {
+        $cookies.remove('loggedin')
+        $location.url('/')
+      }
+
+    }
+
 })
