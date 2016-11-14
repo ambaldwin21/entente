ca7831ff5bc258b2dfbac84c8245dfd6590ac2cc
Anna Baldwin <anna.baldwin21@gmail.com>
2016-11-14 15:31:21 -0700

    app edit for heroku

--- server/app.js
+++ server/app.js
@@ -29,36 +29,14 @@ app.use('/register', register);
 app.use('/signup', signup);
 app.use('/questions', questions);
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+const port = process.env.PORT || 3000;
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+app.listen(port, () => {
+    if (app.get('env') !== 'development') {
+        // eslint-disable-next-line no-console
+        console.log('Listening on port', port);
+    }
+});
 
 
 module.exports = app;
