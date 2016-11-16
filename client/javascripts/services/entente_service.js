a9595b909687ab84b4cc4879acd8341ebc29523d
Anna Baldwin <anna.baldwin21@gmail.com>
2016-11-16 16:27:02 -0700

    abandoning branch perhaps

--- client/javascripts/services/entente_service.js
+++ client/javascripts/services/entente_service.js
@@ -26,6 +26,26 @@ app.service('ententeService', function ($resource) {
 })
 
 //from tutorial
-app.service('SocketService', function ($resource) {
-
-})
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
