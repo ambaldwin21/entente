35226dabdf4629ef7691c17de9ddea0d018c95d1
Anna Baldwin <anna.baldwin21@gmail.com>
2016-11-17 16:12:22 -0700

    chat box style

--- client/javascripts/controllers/SocketController.js
+++ client/javascripts/controllers/SocketController.js
@@ -1,10 +1,4 @@
 app.controller('SocketController', function($scope, $location, ententeService, $cookies, socket) {
-$scope.test = "scope test"
-
-// function AppCtrl($scope, socket) {
-
-  // Socket listeners
-  // ================
 
   socket.on('init', function (data) {
     $scope.name = data.name;
@@ -14,71 +8,6 @@ $scope.test = "scope test"
   socket.on('send:message', function (message) {
     $scope.messages.push(message);
   });
-
-  // socket.on('change:name', function (data) {
-  //   changeName(data.oldName, data.newName);
-  // });
-
-  // socket.on('user:join', function (data) {
-  //   $scope.messages.push({
-  //     user: 'chatroom',
-  //     text: 'User ' + data.name + ' has joined.'
-  //   });
-  //   $scope.users.push(data.name);
-  // });
-
-  // add a message to the conversation when a user disconnects or leaves the room
-  // socket.on('user:left', function (data) {
-  //   $scope.messages.push({
-  //     user: 'chatroom',
-  //     text: 'User ' + data.name + ' has left.'
-  //   });
-  //   var i, user;
-  //   for (i = 0; i < $scope.users.length; i++) {
-  //     user = $scope.users[i];
-  //     if (user === data.name) {
-  //       $scope.users.splice(i, 1);
-  //       break;
-  //     }
-  //   }
-  // });
-
-  // Private helpers
-  // ===============
-
-  // var changeName = function (oldName, newName) {
-  //   // rename user in list of users
-  //   var i;
-  //   for (i = 0; i < $scope.users.length; i++) {
-  //     if ($scope.users[i] === oldName) {
-  //       $scope.users[i] = newName;
-  //     }
-  //   }
-
-  //   $scope.messages.push({
-  //     user: 'chatroom',
-  //     text: 'User ' + oldName + ' is now known as ' + newName + '.'
-  //   });
-  // }
-
-  // Methods published to the scope
-  // ==============================
-
-  // $scope.changeName = function () {
-  //   socket.emit('change:name', {
-  //     name: $scope.newName
-  //   }, function (result) {
-  //     if (!result) {
-  //       alert('There was an error changing your name');
-  //     } else {
-  //
-  //       changeName($scope.name, $scope.newName);
-  //
-  //       $scope.name = $scope.newName;
-  //       $scope.newName = '';
-  //     }
-  //   });
-  // };
 
   $scope.messages = [];
 
@@ -96,6 +25,5 @@ $scope.test = "scope test"
     // clear message box
     $scope.message = '';
   };
-// }
 
 })
